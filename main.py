c4f95cca6d5204bc4c3d9f87fc55dbde57349e13
vamsikrishna2410 <vamsikrishnareddyk02@gmail.com>
2025-05-19 19:43:27 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+from scrapers.pastebin_scraper import scrape_pastebin, load_keywords
+
+if __name__ == "__main__":
+    keywords = load_keywords()
+    results = scrape_pastebin(keywords)
+    print("\nMatched Results:")
+    for url, kw in results:
+        print(f" - Keyword '{kw}' found in {url}")
 
