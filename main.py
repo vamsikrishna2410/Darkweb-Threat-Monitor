3e42ee4be12545648b5524eb509135f79f465b67
vamsikrishna2410 <vamsikrishnareddyk02@gmail.com>
2025-05-20 22:07:50 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
-from scrapers.local_scraper import scrape_local_file
-from scrapers.pastebin_scraper import load_keywords
-
-print("🧠 [MAIN] This is the REAL main.py running")
-
+from scrapers.pastebin_scraper import scrape_pastebin, load_keywords
 
 if __name__ == "__main__":
     keywords = load_keywords()
-    results = scrape_local_file(keywords)
+    results = scrape_pastebin(keywords)
     print("\nMatched Results:")
-    if not results:
-        print("No matches found.")
     for url, kw in results:
         print(f" - Keyword '{kw}' found in {url}")
-
