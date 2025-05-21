0686c51134691159c3ce9814bdc8a9a7eb9ba7c8
vamsikrishna2410 <vamsikrishnareddyk02@gmail.com>
2025-05-20 21:42:44 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-from scrapers.throwbin_scraper import scrape_throwbin, load_keywords
+from scrapers.local_scraper import scrape_local_file, load_keywords
 
 if __name__ == "__main__":
     keywords = load_keywords()
-    results = scrape_throwbin(keywords)
+    results = scrape_local_file(keywords)
     print("\nMatched Results:")
     for url, kw in results:
         print(f" - Keyword '{kw}' found in {url}")
-
