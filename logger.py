8b9bfab0e9152d19480ce2eb75016d2167e1fcc1
vamsikrishna2410 <vamsikrishnareddyk02@gmail.com>
2025-05-20 23:13:49 -0500

    Update logger.py

--- logger.py
+++ logger.py
@@ -1,1 +1,9 @@
+import csv
+from datetime import datetime
+
+def log_to_csv(keyword, url, filename="logs.csv"):
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    with open(filename, mode='a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([timestamp, keyword, url])
 
