2288ed14c5b30696f942d38473c4a90a9590061d
vamsikrishna2410 <vamsikrishnareddyk02@gmail.com>
2025-05-20 21:40:28 -0500

    Update local_scraper.py

--- scrapers/local_scraper.py
+++ scrapers/local_scraper.py
@@ -1,1 +1,11 @@
+def scrape_local_file(keywords):
+    print("[+] Reading from local test file...")
 
+    matches = []
+    with open('test_paste.txt', 'r') as file:
+        content = file.read()
+        for keyword in keywords:
+            if keyword.lower() in content.lower():
+                print(f"[!] Keyword '{keyword}' found in test_paste.txt")
+                matches.append(('test_paste.txt', keyword))
+    return matches
