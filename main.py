e7331d34e447138dc0a110a2ee59a5d072d38ad9
vamsikrishna2410 <vamsikrishnareddyk02@gmail.com>
2025-05-20 22:37:28 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from scrapers.pastebin_scraper import scrape_pastebin, load_keywords
 from alerts.telegram_alert import send_telegram_alert
+BOT_TOKEN = "7807793284:AAFG_t1X2O6jNY3P3RpL-fuNd0wsoLqhbz8"
+CHAT_ID = "965209658"
 
 if __name__ == "__main__":
     keywords = load_keywords()
     results = scrape_pastebin(keywords)
     print("\nMatched Results:")
     for url, kw in results:
-        print(f" - Keyword '{kw}' found in {url}")
+    message = f"⚠️ Keyword *'{kw}'* found in {url}"
+    print(f" - {message}")
+    send_telegram_alert(BOT_TOKEN, CHAT_ID, message)
+
