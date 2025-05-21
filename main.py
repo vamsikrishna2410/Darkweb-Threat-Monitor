c2aac0b52448d8ed05522ffeb71ef920cfa431e4
vamsikrishna2410 <vamsikrishnareddyk02@gmail.com>
2025-05-20 23:14:29 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from scrapers.pastebin_scraper import scrape_pastebin, load_keywords
 from alerts.telegram_alert import send_telegram_alert
+from logger import log_to_csv
+
 BOT_TOKEN = "7807793284:AAFG_t1X2O6jNY3P3RpL-fuNd0wsoLqhbz8"
 CHAT_ID = "965209658"
 
